c5914a241725a406b1672d2a6a71c25331a51cc8
martijno <martijno@717097ad-464a-42c7-ab04-3dfc85175d78>
2018-01-15 13:39:03 +0000

    - Version 0.0.16 (scuba-sc-android).
    git-svn-id: https://svn.code.sf.net/p/scuba/code/trunk@306 717097ad-464a-42c7-ab04-3dfc85175d78

--- scuba_sc_android/src/main/java/net/sf/scuba/smartcards/IsoDepCardService.java
+++ scuba_sc_android/src/main/java/net/sf/scuba/smartcards/IsoDepCardService.java
@@ -26,12 +26,19 @@
 package net.sf.scuba.smartcards;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import net.sf.scuba.smartcards.CardService;
 import net.sf.scuba.smartcards.CardServiceException;
 import net.sf.scuba.smartcards.CommandAPDU;
 import net.sf.scuba.smartcards.ResponseAPDU;
+
 import android.annotation.TargetApi;
+import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcB;
 import android.os.Build;
 
 /**
@@ -45,18 +52,18 @@ import android.os.Build;
  */
 public class IsoDepCardService extends CardService {
 
-  private static final long serialVersionUID = -8123218195642784731L;
+  private static final Logger LOGGER = Logger.getLogger("net.sf.scuba");
 
-  private IsoDep nfc;
+  private IsoDep isoDep;
   private int apduCount;
 
   /**
    * Constructs a new card service.
    * 
-   * @param terminal the card terminal to connect to
+   * @param isoDep the card terminal to connect to
    */
-  public IsoDepCardService(IsoDep nfc) {
-    this.nfc = nfc;
+  public IsoDepCardService(IsoDep isoDep) {
+    this.isoDep = isoDep;
     apduCount = 0;
   }
 
@@ -64,15 +71,18 @@ public class IsoDepCardService extends CardService {
    * Opens a session with the card.
    */
   public void open() throws CardServiceException {
-    if (isOpen()) { return; }
+    if (isOpen()) {
+      return;
+    }
+
     try {
-      nfc.connect();
-      if (!nfc.isConnected()) { 
-        throw new CardServiceException("failed to connect");
+      isoDep.connect();
+      if (!isoDep.isConnected()) {
+        throw new CardServiceException("Failed to connect");
       }
       state = SESSION_STARTED_STATE;
     } catch (IOException e) {
-      e.printStackTrace();
+      LOGGER.log(Level.WARNING, "Failed to connect", e);
       throw new CardServiceException(e.toString());
     }
   }
@@ -81,7 +91,7 @@ public class IsoDepCardService extends CardService {
    * Whether there is an open session with the card.
    */
   public boolean isOpen() {
-    if (nfc.isConnected()) {
+    if (isoDep.isConnected()) {
       state = SESSION_STARTED_STATE;
       return true;
     } else {
@@ -99,10 +109,10 @@ public class IsoDepCardService extends CardService {
    */
   public ResponseAPDU transmit(CommandAPDU ourCommandAPDU) throws CardServiceException {
     try {
-      if (!nfc.isConnected()) {
+      if (!isoDep.isConnected()) {
         throw new CardServiceException("Not connected");
       }
-      byte[] responseBytes = nfc.transceive(ourCommandAPDU.getBytes());
+      byte[] responseBytes = isoDep.transceive(ourCommandAPDU.getBytes());
       if (responseBytes == null || responseBytes.length < 2) {
         throw new CardServiceException("Failed response");
       }
@@ -117,19 +127,36 @@ public class IsoDepCardService extends CardService {
   }
 
   public byte[] getATR() {
-    return null; // FIXME
+    if (isoDep == null) {
+      return null;
+    }
+
+    Tag tag = isoDep.getTag();
+    if (tag == null) {
+      return null;
+    }
+
+    NfcA nfcA = NfcA.get(tag);
+    if (nfcA != null) {
+      return isoDep.getHistoricalBytes();
+    }
+
+    NfcB nfcB = NfcB.get(tag);
+    if (nfcB != null) {
+      return isoDep.getHiLayerResponse();
+    }
+
+    return isoDep.getHistoricalBytes();
   }
 
   @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
   public boolean isExtendedAPDULengthSupported() {
-    if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN ) {
-      return nfc.isExtendedLengthApduSupported();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+      return isoDep.isExtendedLengthApduSupported();
     }
-    int maxTranceiveLength = nfc.getMaxTransceiveLength();
-    if (maxTranceiveLength > 261) {
-      return true;
-    }
-    return false;
+
+    int maxTranceiveLength = isoDep.getMaxTransceiveLength();
+    return maxTranceiveLength > 261;
   }
 
   /**
@@ -137,7 +164,7 @@ public class IsoDepCardService extends CardService {
    */
   public void close() {
     try {
-      nfc.close();
+      isoDep.close();
       state = SESSION_STOPPED_STATE;
     } catch (IOException e) {
       /* Disconnect failed? Fine... */
